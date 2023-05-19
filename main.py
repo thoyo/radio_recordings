1b4853c32c7cb5fc997ba7b29c1f84a3cb8d6ad0
Toni M. del Hoyo <tonidelhoyo@gmail.com>
2023-05-19 21:50:27 +0200

    Add logging

--- main.py
+++ main.py
@@ -10,9 +10,15 @@ import time
 from telegram import Bot
 import asyncio
 from dotenv import load_dotenv
+import logging
+
+logging.basicConfig(format="%(asctime)s %(levelname)-8s %(message)s",
+                    level=logging.INFO,
+                    datefmt="%Y-%m-%d %H:%M:%S")
+logging.info("Starting program")
 
 START_TIME = "06:00"
-DURATION_MINUTES = 0.1  # 10
+DURATION_MINUTES = 10  # 10
 m3u8_file = 'data/rne_r3_main.m3u8'  # Replace with the desired output file path
 
 load_dotenv()
@@ -50,30 +56,41 @@ def download_file(url, output_path):
 
 def job():
     start = datetime.datetime.now()
+    logging.info(f"Starting job at {start}")
+
     ts_list = []
     while True:
         url = 'https://rtvelivestream.akamaized.net/rtvesec/rne/rne_r3_main.m3u8'  # Replace with the URL of the file you want to download
 
+        logging.info("Retrieving m3u8 file")
         download_file(url, m3u8_file)
+        logging.info("m3u8 file retrieved")
+
         ts_sub_list = m3u8_to_ts_list()
         for ts in ts_sub_list:
             if ts not in ts_list:
                 ts_list.append(ts)
+                logging.info(f"Converting segment {ts} to mp3")
+                subprocess.call(['ffmpeg', '-y', '-i', ts, f"data/{ts.split('/')[-1]}.mp3"],
+                                stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        logging.info(f"Segment {ts} converted to mp3")
         if (datetime.datetime.now() - start).total_seconds() / 60 > DURATION_MINUTES:
             break
         time.sleep(10)
 
-    for ts in ts_list:
-        subprocess.call(['ffmpeg', '-y', '-i', ts, f"data/{ts.split('/')[-1]}.mp3"],
-                        stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-
+    logging.info("Concatenating full mp3 file")
     concatenate_mp3_files([f"data/{ts.split('/')[-1]}.mp3" for ts in ts_list], "data/out.mp3")
+    logging.info("Full file concatenated")
 
+    logging.info("Sending file to Telegram")
     loop = asyncio.get_event_loop()
     tasks = [loop.create_task(bot.send_audio(CHAT_ID, "data/out.mp3"))]
     loop.run_until_complete(asyncio.wait(tasks))
+    logging.info("File sent to Telegram")
 
+    logging.info("Deleting data")
     os.system("rm -v data/*")
+
 
 if sys.argv[1] == "manual":
     job()
